e5176643d3fb97908af933219def1bff1cc25a37
davidmr27 <davidmoran464@gmail.com>
2020-08-26 10:45:22 -0500

    actualizacion de contactos realizada por id seleccionado

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, url_for, render_template, request, session
+from flask import Flask, url_for, render_template, request, session, redirect, flash
+from flask_login import LoginManager, login_required, logout_user, login_user
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
 from form import ContactForm
+from user import User
 
 app = Flask(__name__)
 
@@ -14,6 +16,7 @@ app.config['SECRET_KEY'] = 'klasdjfklj232ijr_'
 mysql = MySQL(app)
 
 Bootstrap(app)
+login_manager = LoginManager(app)
 
 @app.route('/insert', methods=['GET','POST'])
 def add_contact():
@@ -22,7 +25,6 @@ def add_contact():
         name = form.name.data
         lName = form.lname.data
         phone = form.phone.data
-        
         cur = mysql.connection.cursor()
         query = "INSERT INTO contactos(nombre, apellido, numero) VALUES (%s,%s,%s)"
         cur.execute(query, (name, lName, phone))
@@ -31,15 +33,50 @@ def add_contact():
         return render_template('success.html')
     return render_template('insert.html', form=form)
 
+@app.route('/update_contact/<id>', methods=['POST'])
+def update_contact(id):
+
+    form = ContactForm()
+    contact = {}
+    if form.validate_on_submit():
+        name = form.name.data
+        lName = form.lname.data
+        phone = form.phone.data
+
+        cur = mysql.connection.cursor()
+        print("{} {} {} {}".format(name, lName, phone, id))
+        query = "UPDATE contactos SET nombre=%s, apellido=%s, numero=%s WHERE id_contact=%s"
+        cur.execute(query, (name, lName, phone,id))
+        mysql.connection.commit()
+        cur.close()
+
+        flash("Actualización Exitosa")   
+        return redirect(url_for('all_contacts'))
+
+@app.route("/edit/<post_id>")
+def edit(post_id):
+    form = ContactForm()
+    cur = mysql.connect.cursor()
+    # query = "SELECT * FROM contactos WHERE id_contact=%s"
+    print(post_id)
+    cur.execute(""" SELECT * FROM contactos WHERE id_contact=%s """,(post_id))
+    contact = cur.fetchall()
+    print(contact)
+    return render_template('edit.html',form=form, contacts=contact[0])
 
 @app.route("/all_contacts")
 def all_contacts():
     """Estamo obteniendo los datos que están en la tabla contactos"""
     #TODO: Filtrar cuando por usuario que inicie sesión
     cur = mysql.connect.cursor()
-    query = "SELECT nombre, apellido, numero FROM contactos;"
+    query = "SELECT * FROM contactos;"
     cur.execute(query)
     contacts = cur.fetchall()
     cur.close()
-    print(contacts)
     return render_template('allcontacts.html', contacts=contacts)
+
+#login manager
+@login_manager.user_loader
+def load_user(user_id):
+    pass
+
