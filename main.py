b1aa4fb9742bc1525efa27e4922453d8f2a284b3
davidmr27 <davidmoran464@gmail.com>
2020-09-01 23:17:09 -0500

    Manejo de inicio de sesion de usuario, se muestra unicamente los contactos de ese usuario

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for, render_template, request, session, redirect, flash
-from flask_login import LoginManager, login_required, logout_user, login_user
+from flask_login import LoginManager, login_required, logout_user, login_user, current_user
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
 from form import ContactForm, LoginForm
@@ -23,16 +23,17 @@ login_manager.login_view = "login"
 @login_required
 def add_contact():
     form = ContactForm()
+    
     if form.validate_on_submit():
         name = form.name.data
         lName = form.lname.data
         phone = form.phone.data
         cur = mysql.connection.cursor()
-        query = "INSERT INTO contactos(nombre, apellido, numero) VALUES (%s,%s,%s)"
-        cur.execute(query, (name, lName, phone))
+        query = "INSERT INTO contactos(nombre, apellido, numero, id_usuario) VALUES (%s,%s,%s,%s)"
+        cur.execute(query, (name, lName, phone, current_user.id))
         mysql.connection.commit()
         cur.close()
-        return render_template('success.html')
+        return redirect(url_for('all_contacts'))
     return render_template('insert.html', form=form)
 
 @app.route('/update_contact/<id>', methods=['POST'])
@@ -61,7 +62,7 @@ def update_contact(id):
 def edit(post_id):
     form = ContactForm()
     cur = mysql.connect.cursor()
-    cur.execute(""" SELECT * FROM contactos WHERE id_contact=%s """,(post_id))
+    cur.execute(""" SELECT * FROM contactos WHERE id_contact=%s """,[post_id])
     contact = cur.fetchall()
     print(contact)
     return render_template('edit.html',form=form, contacts=contact[0])
@@ -85,14 +86,18 @@ def all_contacts():
     """Estamo obteniendo los datos que están en la tabla contactos"""
     #TODO: Filtrar cuando por usuario que inicie sesión
     cur = mysql.connect.cursor()
-    query = "SELECT * FROM contactos;"
-    cur.execute(query)
+    query = """SELECT contactos.id_contact, contactos.nombre, contactos.apellido, numero FROM contactos 
+               LEFT JOIN usuarios ON usuarios.id_usuario = %s;"""
+    cur.execute(query,[current_user.id])
     contacts = cur.fetchall()
     cur.close()
     return render_template('allcontacts.html', contacts=contacts)
 
 @app.route("/login",methods=["GET","POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('all_contacts'))
+
     form = LoginForm()
     if form.validate_on_submit():
         username=form.user.data
@@ -104,8 +109,10 @@ def login():
         cur.close()
 
         if user[0][2] == password and user[0][1] == username:
-
-            login_user(User(username))
+            user = User(user[0][0], username)
+            User.user.append(user)
+            login_user(user)
+            flash("Se inicio sesión exitosamente")
             return redirect(url_for('all_contacts'))
 
         print("user:{} password:{}".format(username,password))
@@ -114,4 +121,13 @@ def login():
 #login manager
 @login_manager.user_loader
 def load_user(user_id):
-    return User(user_id)
+    users = User.user
+    for user in users:
+        if user.id == int(user_id):
+            return user
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
