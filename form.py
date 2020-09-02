a2407a41cb8340e8e09c8406a50ae5fe5e490e61
davidmr27 <davidmoran464@gmail.com>
2020-09-01 23:15:13 -0500

    Agregamos clase para el formulario de login

--- form.py
+++ form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length
 from wtforms.fields.html5 import TelField
 
@@ -8,3 +8,9 @@ class ContactForm(FlaskForm):
     lname = StringField('Apellido', validators=[DataRequired(), Length(min=2,message=("Apellido muy corto"))])
     phone = StringField('Telefono', validators=[DataRequired(), Length(min=8,max=9,message=("Telefono no valido"))])
     submit = SubmitField('Agregar contacto')
+
+
+class LoginForm(FlaskForm):
+    user = StringField('User name', validators=[DataRequired(), Length(min=2, message=("Usuario invalido"))])
+    password = PasswordField('Password Please', validators=[DataRequired(), Length(min=8, message=("Contraseña muy corta"))])
+    submit = SubmitField('Sing up')
